5b3613c8d264e07a78dee6303d0a5ada164ac492
Pedro Queiroz <pedroxsqueiroz@gmail.com>
2018-08-12 19:22:16 -0300

    Adding date parser

---
+++ src/main/java/br/com/pedroXSQueiroz/todoManager/utils/serialization/DateSerializer.java
@@ -0,0 +1,39 @@
+package br.com.pedroXSQueiroz.todoManager.utils.serialization;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class DateSerializer extends StdSerializer<Date> {
+
+	public DateSerializer() {
+		this(null);
+	}
+	
+	public DateSerializer(Class<Date> t) {
+		super(t);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void serialize(	Date value, 
+							JsonGenerator jgen, 
+							SerializerProvider provider) 
+							throws IOException {
+		// TODO Auto-generated method stub
+		
+		Locale languageTag = Locale.forLanguageTag("pt-BR");
+		DateFormat dateInstance = DateFormat.getDateInstance(DateFormat.SHORT, languageTag);
+		String localeDateString = dateInstance.format(value);
+		
+		//jgen.writeStartObject();
+		jgen.writeString(localeDateString);
+		//jgen.writeEndObject();
+	}
+
+}
